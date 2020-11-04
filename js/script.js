793c496e02fce23e5aeac0b24dd50ee7acdf2bab
Elizaveta Fursevich <65729263+elizavetafursevich@users.noreply.github.com>
2020-11-04 13:29:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,5 +9,30 @@ window.addEventListener('scroll', function (e) {
     }
 });
 
+document.getElementById("vid").addEventListener("click", function(e){
+    if (document.getElementsByTagName('video')[0].paused) {  // если видео остановлено, запускаем
+        document.getElementsByTagName('video')[0].play();
+    } else {
+        document.getElementsByTagName('video')[0].pause();
+    }
+});
 
+const planButtums= document.querySelectorAll(".toggle-button");
+for (let i=0; i<planButtums.length;i++){
+
+    planButtums[i].addEventListener("click",function (e){
+        const choosePlan =e.target.getAttribute('data-target');
+        const offerBlock=document.querySelector(".${choosePlan}");
+        planButtums[i].classList.remove("toggle-active");
+        offerBlock.classList.remove("business-block_active");
+
+        for(let j=0; j<planButtums.length;j++){
+            this.add("toggle-active");
+
+            offerBlock.classList.add("business-block_active");
+        }
+
+    });
+
+}
 
